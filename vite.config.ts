b0ff9c9b98fd769e67b5604defd4b3514da2a895
Ravindu Shehara <ravindushehara1234@gmail.com>
2026-04-17 18:27:55 +0530

    udpdate

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,3 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  // This override tells TanStack Start to generate static files
-  // so that Vercel can find an index.html file.
-  start: {
-    prerender: {
-      routes: ["/"]
-    }
-  }
-});
+export default defineConfig();
