89a46e9525a935b30a6798ad48cb46977cce5911
Ravindu Shehara <ravindushehara1234@gmail.com>
2026-04-17 18:42:41 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig();
+export default defineConfig({
+  // Manually adding Nitro support to ensure Vercel detection works perfectly
+  vite: {
+    plugins: []
+  }
+});
